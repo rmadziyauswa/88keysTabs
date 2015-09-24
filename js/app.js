4787522c4ff8f6963ba838ed8553199f78b4b579
rmadziyauswa <rmadziyauswa@gmail.com>
2015-09-24 15:08:23 +0200

    Addition of the home and view templates

--- js/app.js
+++ js/app.js
@@ -1,6 +1,62 @@
-(function(){
+(function (){
 
-	var app = angular.module("88keys",[]);
+	var app = angular.module("keyTabs", ['ngRoute']);
+
+	
+	app.config(['$routeProvider',function($routeProvider){
+		$routeProvider.when(
+			"/home",
+			{
+				templateUrl : "templates/home.html",
+				controller : "songsCtrl"
+			}
+		)
+		.when("/view/:id",
+		{
+			templateUrl : "templates/view.html",
+			controller : "singleSongCtrl"
+		}
+		)
+		.otherwise({redirectTo : "/home"});
+		
+		
+	}]);
+
+
+	app.controller("songsCtrl", ["$scope", "$http", function ($scope, $http) {
+
+
+		$http.get('data.json').success(function (response) {
+			$scope.songs = response;
+		});
+
+		$scope.clearSearchTabs = function(){
+			$scope.searchtabs = "";
+		};
+
+	}]);
+
+
+	app.controller("singleSongCtrl", ["$scope", "$http","$routeParams", function ($scope, $http,$routeParams) {
+
+		
+		$http.get('data.json').success(function (response) {
+			$scope.songs = response;
+
+			$scope.songs.forEach(function(item){
+				
+
+				if( item.id === $routeParams.id)
+				{
+					$scope.song = item;
+
+				}
+			});
+		});
+
+
+	}]);
+
 
 
 
